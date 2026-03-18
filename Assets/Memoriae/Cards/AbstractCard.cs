1b5922ce7876a8bd4fb4dd70b345cedc52a5e1ad
Matthew <tatoba2351@gmail.com>
2026-03-18 23:47:46 +0800

    feat: make abstract card, the base of all cards

---
+++ Assets/Memoriae/Cards/AbstractCard.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Memoriae
+{
+    public abstract class AbstractCard
+    {
+        /// <summary>
+        /// 卡牌名稱（本地化鍵值）
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// 卡牌描述（本地化鍵值）
+        /// </summary>
+        public virtual string TargetDescription { get; }
+
+        /// <summary>
+        /// 卡牌描述（本地化鍵值）
+        /// </summary>
+        public virtual string CostDescription { get; }
+
+        /// <summary>
+        /// 卡牌描述（本地化鍵值）
+        /// </summary>
+        public abstract string EffectDescription { get; }
+
+        /// <summary>
+        /// 卡牌圖片路徑
+        /// </summary>
+        public abstract string ImagePath { get; }
+
+        /// <summary>
+        /// 卡牌類型（例：battlefield、spell、etc.）
+        /// </summary>
+        public abstract string CardType { get; }
+
+        /// <summary>
+        /// 卡牌唯一識別碼
+        /// </summary>
+        public abstract string Id { get; }
+
+        public abstract void UseCard();
+    }
+}
