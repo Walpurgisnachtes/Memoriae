5e0a5e4d76748815336beb7d200b3834d396671e
Matthew <tatoba2351@gmail.com>
2026-03-18 16:40:12 +0800

    feat: bind piece status to piece

--- Assets/Memoriae/Character/Piece.cs
+++ Assets/Memoriae/Character/Piece.cs
@@ -7,10 +7,12 @@ namespace Memoriae
     public class Piece
     {
         public string Name { get; private set; }
+        public PieceStats Stats { get; private set; }
 
-        public Piece(string name)
+        public Piece(string name, PieceStats stats = null)
         {
             Name = name;
+            Stats = stats ?? new PieceStats();
         }
     }
 }
