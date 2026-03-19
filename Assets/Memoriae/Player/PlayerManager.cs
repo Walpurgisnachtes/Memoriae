e20f26709f5eb6c1ff3711abbd65f20c23db143f
Matthew <tatoba2351@gmail.com>
2026-03-19 23:16:42 +0800

    chore: create file PlayerManager

---
+++ Assets/Memoriae/Player/PlayerManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Memoriae
+{
+    public class PlayerManager
+    {
+        public Object[] StatusEffects;
+    }
+}
