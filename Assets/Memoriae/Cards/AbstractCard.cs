db63dd6c8b62a327094476fe569bbb4b9c6f6b08
Matthew <tatoba2351@gmail.com>
2026-03-19 23:35:17 +0800

    chore: protect card fields that should not be changed out of card;  now initialize a guid id field

--- Assets/Memoriae/Cards/AbstractCard.cs
+++ Assets/Memoriae/Cards/AbstractCard.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,37 +10,37 @@ namespace Memoriae
         /// <summary>
         /// 卡牌名稱（本地化鍵值）
         /// </summary>
-        public abstract string Name { get; }
+        public abstract string Name { get; protected set; }
 
         /// <summary>
         /// 卡牌描述（本地化鍵值）
         /// </summary>
-        public virtual string TargetDescription { get; }
+        public virtual string TargetDescription { get; protected set; }
 
         /// <summary>
         /// 卡牌描述（本地化鍵值）
         /// </summary>
-        public virtual string CostDescription { get; }
+        public virtual string CostDescription { get; protected set; }
 
         /// <summary>
         /// 卡牌描述（本地化鍵值）
         /// </summary>
-        public abstract string EffectDescription { get; }
+        public abstract string EffectDescription { get; protected set; }
 
         /// <summary>
         /// 卡牌圖片路徑
         /// </summary>
-        public abstract string ImagePath { get; }
+        public abstract string ImagePath { get; protected set; }
 
         /// <summary>
         /// 卡牌類型（例：battlefield、spell、etc.）
         /// </summary>
-        public abstract string CardType { get; }
+        public abstract string CardType { get; set; }
 
         /// <summary>
         /// 卡牌唯一識別碼
         /// </summary>
-        public abstract string Id { get; }
+        public Guid Id { get; private set; } = Guid.NewGuid();
 
         public abstract void UseCard();
     }
