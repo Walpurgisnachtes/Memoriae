0cd83d31ca112c54d71953ed3845f3920a7343bb
Matthew <tatoba2351@gmail.com>
2026-03-19 23:43:04 +0800

    fix: fix the card detail modification problem in derived card

--- Assets/Memoriae/Cards/Imperial/EightCorners.cs
+++ Assets/Memoriae/Cards/Imperial/EightCorners.cs
@@ -1,25 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace Memoriae
 {
     public class EightCorners : AbstractCard
     {
-        public override string Name => "LOC_CARD_100001_NAME";
+        public override string CardType { get; set; } = "attack";
 
-        public override string TargetDescription => "LOC_CARD_100001_TARGET_DESC";
-
-        public override string EffectDescription => "LOC_CARD_100001_EFFECT_DESC";
-
-        public override string ImagePath => "100001.png";
-
-        public override string CardType => "attack";
-
-        public override string Id => "100001";
+        public EightCorners()
+        {
+            SetName("LOC_CARD_100001_NAME");
+            SetTargetDescription("LOC_CARD_100001_TARGET_DESC");
+            SetEffectDescription("LOC_CARD_100001_EFFECT_DESC");
+            SetImagePath("100001.png");
+        }
 
         public override void UseCard()
         {
-            throw new System.NotImplementedException();
+            // 實現卡牌效果
         }
     }
 }
